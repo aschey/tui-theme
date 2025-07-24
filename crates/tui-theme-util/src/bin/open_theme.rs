079f5213fe45a2295b519f75877f101bbcf0a9f8
Austin Schey <aschey13@gmail.com>
2025-07-23 19:15:42 -0700

    add theme picker

--- crates/tui-theme-util/src/bin/open_theme.rs
+++ crates/tui-theme-util/src/bin/open_theme.rs
@@ -1,12 +1,34 @@
-use std::fs::File;
+use std::ffi::OsStr;
+use std::fs::{self, File};
 use std::io::{self, Read};
 
 use base64::Engine;
 use base64::prelude::BASE64_URL_SAFE_NO_PAD;
+use dialoguer::Select;
+use dialoguer::theme::ColorfulTheme;
 use tui_theme::Color;
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("themes/catppuccin.css").unwrap();
+    let theme_files: Vec<_> = fs::read_dir("themes")
+        .unwrap()
+        .filter_map(|f| f.ok())
+        .filter(|f| f.path().is_file() && f.path().extension() == Some(OsStr::new("css")))
+        .map(|f| {
+            f.file_name()
+                .to_string_lossy()
+                .to_string()
+                .replace(".css", "")
+        })
+        .collect();
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select theme")
+        .default(0)
+        .items(&theme_files[..])
+        .interact()
+        .unwrap();
+
+    let mut file = File::open(format!("themes/{}.css", theme_files[selection])).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let lines = contents.split("\n").filter_map(|l| {
@@ -29,16 +51,14 @@ fn main() -> io::Result<()> {
             if name.ends_with(&color_suffix) {
                 let name = name.replace("--color-", "").replace(&color_suffix, "");
                 let color: Color = val.parse().unwrap();
-                println!("color {color:?}");
                 let hex = color.to_hex_fg();
-                println!("hex {hex}");
                 Some(format_color(&name, &hex, color_num))
             } else {
                 None
             }
         })
         .collect();
-    ::open::that_detached(format!(
+    open::that(format!(
         "https://www.tints.dev/palette/v1:{}",
         BASE64_URL_SAFE_NO_PAD.encode(formatted.join("~"))
     ))
